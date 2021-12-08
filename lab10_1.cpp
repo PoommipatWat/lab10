f1f5c0a384c95bbbb7ca516b984794195ce10b42
PoommipatWat <poommipat_wat@cmu.ac.th>
2021-12-08 14:02:44 +0700

    "lab10_1.cpp completed"

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,9 +3,15 @@
 using namespace std;
 
 int main(){	
+	double money , interest , pay;
+	int i = 1;
 	cout << "Enter initial loan: ";
+	cin >> money;
 	cout << "Enter interest rate per year (%): ";
+	cin >> interest;
+	interest /= 100;
 	cout << "Enter amount you can pay per year: ";
+	cin >> pay;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +26,23 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+	while(money > 0){
+		cout << fixed << setprecision(2); 
+		cout << setw(13) << left << i; 
+		cout << setw(13) << left << money;
+		cout << setw(13) << left << money*interest;
+		money = (money*interest)+money;
+		cout << setw(13) << left << money;
+		if (money < pay){
+			pay = money;
+		}
+		cout << setw(13) << left << pay;
+		money -= pay;
+		cout << setw(13) << left << money;
+		cout << "\n";
+		i++;
+	}
+
+
 	return 0;
 }
